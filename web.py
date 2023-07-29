dbc4c104e460de73428625becddebb0239715f4d
Asif Ahsan <44509822+asifahsaan@users.noreply.github.com>
2023-07-29 06:50:14 +0500

    Add files via upload

---
+++ web.py
@@ -0,0 +1,119 @@
+# app.py
+import streamlit as st
+import time
+import numpy as np
+
+# st.title('Uber pickups in NYC')
+
+
+
+# def main():
+#     st.title("My Streamlit App")
+#     st.write("Hello, Streamlit!")
+
+#     st.sidebar.title("Sidebar Title")
+#     st.sidebar.write("Hello, Streamlit!")
+    # Add a slider to the sidebar for user input on how many rows they want to see from
+    # the dataframe.
+    # progress_bar = st.sidebar.progress(0)
+    # status_text = st.sidebar.empty()
+    # last_rows = np.random.randn(1, 1)
+    # chart = st.line_chart(last_rows)
+
+    # for i in range(1, 101):
+    #     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    #     status_text.text("%i%% Complete" % i)
+    #     chart.add_rows(new_rows)
+    #     progress_bar.progress(i)
+    #     last_rows = new_rows
+    #     time.sleep(0.05)
+
+    # progress_bar.empty()
+    # # Streamlit widgets automatically run the script from top to bottom. Since
+    #     # this button is not connected to any other logic, it just causes a plain
+    #     # rerun.
+    # st.button("Re-run")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
+# @st.cache_data
+# def get_UN_data():
+#     AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
+#     df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
+#     return df.set_index("Region")
+
+# try:
+#     df = get_UN_data()
+#     countries = st.multiselect(
+#         "Choose countries", list(df.index), ["China", "United States of America"]
+#     )
+#     if not countries:
+#         st.error("Please select at least one country.")
+#     else:
+#         data = df.loc[countries]
+#         data /= 1000000.0
+#         st.write("### Gross Agricultural Production ($B)", data.sort_index())
+
+#         data = data.T.reset_index()
+#         data = pd.melt(data, id_vars=["index"]).rename(
+#             columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
+#         )
+#         chart = (
+#             alt.Chart(data)
+#             .mark_area(opacity=0.3)
+#             .encode(
+#                 x="year:T",
+#                 y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
+#                 color="Region:N",
+#             )
+#         )
+#         st.altair_chart(chart, use_container_width=True)
+# except URLError as e:
+#     st.error(
+#         """
+#         **This demo requires internet access.**
+#         Connection error: %s
+#     """
+#         % e.reason
+#     )
